7bd6b348a61e9b4670d775b87c0c6d3128e0a34b
신수진 <shinsj5037@gmail.com>
2024-06-24 23:43:44 +0900

    perf: 갤러리 이미지 슬라이드 개선

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -69,7 +69,8 @@ export default function GalleryPage() {
     },
   ]);
 
-  const handleClick = (index: number) => {
+  const handleClick = (e: React.MouseEvent, index: number) => {
+    e.stopPropagation();
     console.log(index);
     setImgList((prev) =>
       prev.map((item, itemIndex) => ({
@@ -79,48 +80,62 @@ export default function GalleryPage() {
       }))
     );
 
+    let prevIdx3 = index - 3;
     let prevIdx2 = index - 2;
     let prevIdx1 = index - 1;
     let nextIdx1 = index + 1;
     let nextIdx2 = index + 2;
+    let nextIdx3 = index + 3;
     if (index === 0) {
+      prevIdx3 = imgList.length - 3;
       prevIdx2 = imgList.length - 2;
       prevIdx1 = imgList.length - 1;
     }
     if (index === 1) {
+      prevIdx3 = imgList.length - 2;
       prevIdx2 = imgList.length - 1;
+    }
+    if (index === 2) {
+      prevIdx3 = imgList.length - 1;
+    }
+    if (index === imgList.length - 3) {
+      nextIdx3 = 0;
     }
     if (index === imgList.length - 2) {
       nextIdx2 = 0;
+      nextIdx3 = 1;
     }
     if (index === imgList.length - 1) {
       nextIdx1 = 0;
       nextIdx2 = 1;
+      nextIdx3 = 2;
     }
 
     setImgList((prev) => {
-      prev[prevIdx2].idx = 1;
-      prev[prevIdx1].idx = 2;
-      prev[index].idx = 3;
-      prev[nextIdx1].idx = 4;
-      prev[nextIdx2].idx = 5;
+      prev[prevIdx3].idx = 1;
+      prev[prevIdx2].idx = 2;
+      prev[prevIdx1].idx = 3;
+      prev[index].idx = 4;
+      prev[nextIdx1].idx = 5;
+      prev[nextIdx2].idx = 6;
+      prev[nextIdx3].idx = 7;
       return prev;
     });
   };
   return (
-    <div className="body">
-      <div className="container">
-        <div className="cards">
+    <div className='body'>
+      <div className='container'>
+        <div className='cards'>
           {imgList.map((item, index) => {
             return (
               <div key={index}>
                 <input
-                  className="card-radio"
-                  type="radio"
-                  name="slider"
+                  className='card-radio'
+                  type='radio'
+                  name='slider'
                   id={`item-${index + 1}`}
                   defaultChecked={item.isChecked}
-                  onClick={() => handleClick(index)}
+                  onClick={(e) => handleClick(e, index)}
                 ></input>
                 <label
                   className={`card ${item.isChecked && "center"} idx-${
@@ -130,7 +145,7 @@ export default function GalleryPage() {
                   id={`image-${index + 1}`}
                 >
                   <Image src={item.src} alt={`image-${index + 1}`} />
-                  <p className="bg-white text-black p-4">
+                  <p className='bg-white text-black p-4'>
                     {index} - {item.isChecked} - {item.idx} - {item.date}
                   </p>
                 </label>
